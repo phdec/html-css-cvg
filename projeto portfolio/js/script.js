b05ea664f76daae3a48faf169e32ef7e2cfd4d79
Pedro Henrique <pedr0101we@gmail.com>
2026-05-21 19:04:35 -0300

    mudança de tema

--- projeto portfolio/js/script.js
+++ projeto portfolio/js/script.js
@@ -1,1 +1,18 @@
-const botaoTema = document.getElementById('botao-tema');
+const botaoTema = document.getElementById('botao-tema')
+const body = document.body
+
+const temaNormal = localStorage.getItem('tema')
+tema(temaNormal === 'escuro')
+
+function tema(temaAtual){
+    if(temaAtual === true){
+        body.classList.add('escuro')
+    }
+    else{body.classList.remove('escuro')}
+}
+
+botaoTema.addEventListener('click', () => {
+    const escuro = body.classList.toggle('escuro')
+    tema(escuro)
+    localStorage.setItem('tema', escuro ? 'escuro' : 'claro')
+})
