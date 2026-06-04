ee7728bbfc8a1428a2c76dce0abe90ffa471cae6
Pedro Henrique <pedr0101we@gmail.com>
2026-06-04 03:51:52 -0300

    media queries

--- projeto portfolio/js/script.js
+++ projeto portfolio/js/script.js
@@ -16,3 +16,20 @@ botaoTema.addEventListener('click', () => {
     tema(escuro)
     localStorage.setItem('tema', escuro ? 'escuro' : 'claro')
 })
+
+const navLinks = document.querySelectorAll('ul a.link')
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault()
+        const target = document.querySelector(this.getAttribute('href'))
+        if(target){
+            const headerHeight = document.querySelector('header').offsetHeight
+            const targetPosition = target.offsetTop - headerHeight - 20
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            })
+        }
+    })
+})
